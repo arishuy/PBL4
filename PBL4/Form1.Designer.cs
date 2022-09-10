e4d247c20b70585514ba0e87e9bc0bd13781a8e0
arishuy <huybui19898@gmail.com>
2022-09-10 12:47:10 +0700

    Update

--- PBL4/Form1.Designer.cs
+++ PBL4/Form1.Designer.cs
@@ -32,11 +32,28 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSubnet = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvResult = new System.Windows.Forms.ListView();
+            this.ip = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hostname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.status = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(254, 36);
+            this.button1.Location = new System.Drawing.Point(255, 67);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -46,7 +63,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(133, 36);
+            this.textBox1.Location = new System.Drawing.Point(131, 67);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 1;
@@ -63,23 +80,169 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(360, 39);
+            this.label2.Location = new System.Drawing.Point(353, 70);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 16);
             this.label2.TabIndex = 3;
             this.label2.Text = "Status";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(171, 383);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Scan IP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Scan Network Address";
+            // 
+            // txtSubnet
+            // 
+            this.txtSubnet.Location = new System.Drawing.Point(65, 384);
+            this.txtSubnet.Name = "txtSubnet";
+            this.txtSubnet.Size = new System.Drawing.Size(100, 22);
+            this.txtSubnet.TabIndex = 8;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(314, 390);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(44, 16);
+            this.lblStatus.TabIndex = 9;
+            this.lblStatus.Text = "label4";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(791, 292);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 36);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(711, 334);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(280, 138);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "IP";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Host Name";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Status";
+            // 
+            // lvResult
+            // 
+            this.lvResult.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ip,
+            this.hostname,
+            this.status});
+            this.lvResult.HideSelection = false;
+            this.lvResult.Location = new System.Drawing.Point(65, 139);
+            this.lvResult.Name = "lvResult";
+            this.lvResult.Size = new System.Drawing.Size(585, 210);
+            this.lvResult.TabIndex = 12;
+            this.lvResult.UseCompatibleStateImageBehavior = false;
+            this.lvResult.View = System.Windows.Forms.View.Details;
+            // 
+            // ip
+            // 
+            this.ip.Text = "IP";
+            this.ip.Width = 142;
+            // 
+            // hostname
+            // 
+            this.hostname.Text = "Host Name";
+            this.hostname.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.hostname.Width = 296;
+            // 
+            // status
+            // 
+            this.status.Text = "Status";
+            this.status.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.status.Width = 143;
+            // 
+            // txtIP
+            // 
+            this.txtIP.Enabled = false;
+            this.txtIP.Location = new System.Drawing.Point(131, 39);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(100, 22);
+            this.txtIP.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(62, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Ping IP";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(65, 355);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(585, 23);
+            this.progressBar.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(264, 390);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Status";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1020, 496);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.lvResult);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtSubnet);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIP);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "My Basic Network Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,6 +254,23 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSubnet;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ListView lvResult;
+        private System.Windows.Forms.ColumnHeader ip;
+        private System.Windows.Forms.ColumnHeader hostname;
+        private System.Windows.Forms.ColumnHeader status;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- PBL4/Form1.cs
+++ PBL4/Form1.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 using System.Windows.Forms;
 namespace PBL4
 {
@@ -26,5 +32,92 @@ namespace PBL4
             label2.Text = PR.Status.ToString();
             p1.Dispose();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string subnet = txtSubnet.Text;
+            progressBar.Maximum = 254;
+            progressBar.Value = 0;
+            lvResult.Items.Clear();
+
+            Task.Factory.StartNew(new Action(() =>
+            {
+                for (int i = 2; i < 255; i++)
+                {
+                    string ip = $"{subnet}.{i}";
+                    Ping ping = new Ping();
+                    PingReply reply = ping.Send(ip, 100);
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        progressBar.BeginInvoke(new Action(() =>
+                        {
+                            try
+                            {
+                                IPHostEntry host = Dns.GetHostEntry(IPAddress.Parse(ip));
+                                lvResult.Items.Add(new ListViewItem(new String[] { ip, host.HostName, "Up" }));
+                            }
+                            catch
+                            {
+                                MessageBox.Show($"Couldn't retrieve hostname from {ip}", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                            progressBar.Value += 1;
+                            lblStatus.ForeColor = Color.Blue;
+                            lblStatus.Text = $"Scanning: {ip}";
+                            if (progressBar.Value == 253)
+                                lblStatus.Text = "Finished";
+                        }));
+                    }
+                    else
+                    {
+                        progressBar.BeginInvoke(new Action(() =>
+                        {
+                            progressBar.Value += 1;
+                            lblStatus.ForeColor = Color.DarkGray;
+                            lblStatus.Text = $"Scanning: {ip}";
+                            lvResult.Items.Add(new ListViewItem(new String[] { ip, "", "Down" }));
+                            if (progressBar.Value == 253)
+                            { lblStatus.Text = "Finished";
+                                progressBar.Value = 0;
+                            }
+                        }));
+                    }
+                }
+            }));
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+
+            //throw new InvalidOperationException("Unable to find DNS Address");
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface adapter in adapters)
+            {
+
+                IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
+                IPAddressCollection dnsServers = adapterProperties.DnsAddresses;
+                if (dnsServers.Count > 0)
+                {
+                    Console.WriteLine(adapter.Description);
+                    foreach (IPAddress dns in dnsServers)
+                    {
+                        Console.WriteLine("  DNS Servers ............................. : {0}",
+                            dns.ToString());
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                var host = Dns.GetHostEntry(Dns.GetHostName());
+                foreach (var ip in host.AddressList)
+                {
+                    if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                    txtIP.Text = ip.ToString();
+                }
+                }
+            }
+        }
     }
-}
