bf11545f062c33e4facd8af728829c9d40a11142
HungNG2k2 <hung200220@gmail.com>
2022-09-11 10:22:51 +0700

    add tracertform

---
+++ PBL4/TracertForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace PBL4
+{
+    partial class TracertForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.HopID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Address = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.HostName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ReplyTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ReplyStatus = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(220, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.HopID,
+            this.Address,
+            this.HostName,
+            this.ReplyTime,
+            this.ReplyStatus});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(34, 60);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(757, 299);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(54, 390);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // HopID
+            // 
+            this.HopID.Text = "HopID";
+            // 
+            // Address
+            // 
+            this.Address.Text = "Address";
+            // 
+            // HostName
+            // 
+            this.HostName.Text = "HostName";
+            // 
+            // ReplyTime
+            // 
+            this.ReplyTime.Text = "ReplyTime";
+            // 
+            // ReplyStatus
+            // 
+            this.ReplyStatus.Text = "ReplyStatus";
+            // 
+            // TracertForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(872, 485);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "TracertForm";
+            this.Text = "Tracert";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ColumnHeader HopID;
+        private System.Windows.Forms.ColumnHeader Address;
+        private System.Windows.Forms.ColumnHeader HostName;
+        private System.Windows.Forms.ColumnHeader ReplyTime;
+        private System.Windows.Forms.ColumnHeader ReplyStatus;
+    }
+}
---
+++ PBL4/TracertForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBL4
+{
+    public partial class TracertForm : Form
+    {
+        public TracertForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach(TracertEntry tracertEntry in Tracert(textBox1.Text, 100, 100))
+                {
+                    listView1.Items.Add(new ListViewItem(new String[]
+                    {
+                        tracertEntry.HopID.ToString(),
+                        tracertEntry.Address.ToString(),
+                        tracertEntry.Hostname.ToString(),
+                        tracertEntry.ReplyTime.ToString(),
+                        tracertEntry.ReplyStatus.ToString(),
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private IEnumerable<TracertEntry> Tracert(string ipAddress, int maxHops, int timeout)
+        {
+            // Ensure that the argument address is valid.
+            if (!IPAddress.TryParse(ipAddress, out IPAddress address))
+                throw new ArgumentException(string.Format("{0} is not a valid IP address.", ipAddress));
+
+            // Max hops should be at least one or else there won't be any data to return.
+            if (maxHops < 1)
+                throw new ArgumentException("Max hops can't be lower than 1.");
+
+            // Ensure that the timeout is not set to 0 or a negative number.
+            if (timeout < 1) throw new ArgumentException("Timeout value must be higher than 0."); 
+            Ping ping = new Ping(); 
+            PingOptions pingOptions = new PingOptions(1, true); 
+            Stopwatch pingReplyTime = new Stopwatch(); 
+            PingReply reply;
+            do
+            {
+                pingReplyTime.Start(); 
+                reply = ping.Send(address, timeout, new byte[] { 0 }, pingOptions); 
+                pingReplyTime.Stop(); 
+                string hostname = string.Empty; 
+                if (reply.Address != null)
+                {
+                    try
+                    {
+                        IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName()); hostname = ipHostInfo.HostName;                 
+                                                                                                                                      
+                    }
+                    catch (SocketException) { /* No host available for that address. */ }
+                }
+
+                // Return out TracertEntry object with all the information about the hop.
+                yield return new TracertEntry()
+                {
+                    HopID = pingOptions.Ttl,
+                    Address = reply.Address == null ? "N/A" : reply.Address.ToString(),
+                    Hostname = hostname,
+                    ReplyTime = pingReplyTime.ElapsedMilliseconds,
+                    ReplyStatus = reply.Status.ToString(),
+                };
+                pingOptions.Ttl++;
+                pingReplyTime.Reset();
+            }
+            while (reply.Status != IPStatus.Success && pingOptions.Ttl <= maxHops);
+        }
+    }
+}
