37311bb9678cfbe0c6f11597b2f33ef783eb0658
Huy Nguyễn Hữu <nguyenhuuhuya1@gmail.com>
2022-09-30 11:24:11 +0700

    add dns query

---
+++ PBL4/DNSQueryForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace PBL4
+{
+    partial class DNSQueryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbdns = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbdns
+            // 
+            this.tbdns.Location = new System.Drawing.Point(104, 26);
+            this.tbdns.Multiline = true;
+            this.tbdns.Name = "tbdns";
+            this.tbdns.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbdns.Size = new System.Drawing.Size(384, 349);
+            this.tbdns.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(192, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(328, 393);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // DNSQueryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbdns);
+            this.Name = "DNSQueryForm";
+            this.Text = "DNSQueryForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbdns;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
--- PBL4/Form1.cs
+++ PBL4/Form1.cs
@@ -21,10 +21,112 @@ namespace PBL4
         {
             label2.Text = "";
             String myping = textBox1.Text;
-            Ping p1 = new Ping();
-            PingReply PR = p1.Send(myping);
-            label2.Text = PR.Status.ToString();
-            p1.Dispose();
+            if (myping == "")
+            {
+                MessageBox.Show("Please enter a valid IP address");
+            }
+            else
+            {
+                Ping p1 = new Ping();
+                PingReply PR = p1.Send(myping);
+                if (PR.Status == IPStatus.Success)
+                {
+                    label2.Text = "Ping to " + myping + " was successful";
+                }
+                else
+                {
+                    label2.Text = "Ping to " + myping + " was unsuccessful";
+                }
+                label2.ForeColor = Color.Green;
+                //label2.Text = PR.Status.ToString();
+                p1.Dispose();
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string subnet = txtIP.Text.Substring(0, txtIP.Text.LastIndexOf("."));
+            progressBar.Maximum = 254;
+            progressBar.Value = 0;
+            lvResult.Items.Clear();
+            lbStatus.ForeColor = Color.Blue;
+            lbStatus.Text = "Scanning...";
+            Task.Factory.StartNew(new Action(() =>
+            {
+                Parallel.For(2, 255, (i) =>
+                //for (int i = 2; i < 255; i++)
+                {
+                    int timeout = 500;
+                    string ip = $"{subnet}.{i}";
+                    Ping ping = new Ping();
+                    PingOptions pingOptions = new PingOptions(1, true);
+                    PingReply reply = ping.Send(ip, timeout, new byte[] { 0 }, pingOptions);
+
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        progressBar.BeginInvoke(new Action(() =>
+                        {
+
+                            try
+                            {
+                                IPHostEntry host = Dns.GetHostEntry(IPAddress.Parse(ip));
+                                lvResult.Items.Add(new ListViewItem(new String[] { ip, host.HostName, "Active" }));
+                            }
+                            catch (SocketException ex)
+                            {
+                                lvResult.Items.Add(new ListViewItem(new String[] { ip, "Unknown", "Active" }));
+                            }
+                            progressBar.Value += 1;
+
+                            if (progressBar.Value == 253)
+                            {
+                                lbStatus.ForeColor = Color.Green;
+                                lbStatus.Text = "Finished";
+                            }
+
+                        }));
+                    }
+                    else
+                    {
+                        progressBar.BeginInvoke(new Action(() =>
+                        {
+                            progressBar.Value += 1;
+
+                            //lvResult.Items.Add(new ListViewItem(new String[] { ip, "", "Down" }));
+                            if (progressBar.Value == 253)
+                            {
+                                lbStatus.ForeColor = Color.Green;
+                                lbStatus.Text = "Finished";
+                                progressBar.Value = 0;
+                            }
+                        }));
+                    }
+                });
+            }));
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DNSQueryForm dnsQueryForm = new DNSQueryForm();
+            dnsQueryForm.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    txtIP.Text = ip.ToString();
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            TracertForm tracert = new TracertForm();
+            tracert.Show();
         }
     }
 }
