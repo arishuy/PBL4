2e5664821499a01d10967ed1e3e80aea92808b0c
arishuy <huybui19898@gmail.com>
2022-11-26 14:28:14 +0700

    Thread

--- PBL4/IPScanner.cs
+++ PBL4/IPScanner.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PBL4
@@ -57,10 +58,12 @@ namespace PBL4
             }
             ipCombo.SelectedIndex = 0;
         }
-        private void ScanIP(IPAddress subnet, int count, int timeout, IPAddress ip)
+        private async void ScanIP(IPAddress subnet, int count, int timeout, IPAddress ip)
         {
             IPAddress NA = GetNetWorkAddress(subnet, ip);
+            Console.WriteLine(NA);
             IPAddress BA = GetBroadCastAddress(subnet, ip);
+            Console.WriteLine(BA);
             string[] startIPString = NA.ToString().Split('.');
             int[] startIP = Array.ConvertAll<string, int>(startIPString, int.Parse); //Change string array to int array
             string[] endIPString = BA.ToString().Split('.');
@@ -69,59 +72,64 @@ namespace PBL4
             PingReply reply;
             IPAddress addr;
             IPHostEntry host;
-            //progressBar.Minimum = 0;
-            //progressBar.Maximum = 255 * (endIP[2] - startIP[2] + 1);
+            progressBar.Minimum = 0;
+            progressBar.Maximum = 1000;
             //Loops through the IP range, maxing out at 255
-            for (int i = startIP[2]; i <= endIP[2]; i++)
-            { //3rd octet loop
-                for (int y = startIP[3]; y <= 255; y++)
-                { //4th octet loop
-                    myThread = new Thread(() =>
-                    {
-                        string ipAddress = startIP[0] + "." + startIP[1] + "." + i + "." + y; //Convert IP array back into a string
-                        string endIPAddress = endIP[0] + "." + endIP[1] + "." + endIP[2] + "." + (endIP[3] + 1); // +1 is so that the scanning stops at the correct range
+            await Task.Run(() =>
+            {
+                for (int i = startIP[2]; i <= endIP[2]; i++)
+                { //3rd octet loop
+                    for (int y = startIP[3]; y <= 255; y++)
+                    { //4th octet loop
+                        Thread myThread = new Thread(() =>
+                        {
+                            string ipAddress = startIP[0] + "." + startIP[1] + "." + i + "." + y; //Convert IP array back into a string
+                            string endIPAddress = endIP[0] + "." + endIP[1] + "." + endIP[2] + "." + endIP[3]; // +1 is so that the scanning stops at the correct range
 
-                        //If current IP matches final IP in range, break
-                    
-                        myPing = new Ping();
-                        try
-                        {
-                            reply = myPing.Send(ipAddress, timeout); //Ping IP address with 500ms timeout
-                        }
-                        catch (Exception ex)
-                        {
-                            return;
-                        }
-                        //lbStatus.ForeColor = System.Drawing.Color.Green; //Set status label for current IP address
-                        //lbStatus.Text = "Scanning: " + ipAddress;
-                        //Log pinged IP address in listview
-                        //Grabs DNS information to obtain system info
-                        if (reply.Status == IPStatus.Success)
-                        {
+                            //If current IP matches final IP in range, break
+
+                            myPing = new Ping();
                             try
                             {
-                                addr = IPAddress.Parse(ipAddress);
-                                host = Dns.GetHostEntry(addr);
-                                data.Rows.Add(addr, host.HostName, "Active");
+                                reply = myPing.Send(ipAddress, timeout); //Ping IP address with 500ms timeout
                             }
-                            catch
+                            catch (Exception ex)
                             {
-
-                                data.Rows.Add(IPAddress.Parse(ipAddress), "Unknown", "Active");
+                                return;
                             }
-                        }
-                        //else
-                        //{
-                        //    data.Rows.Add(ipAddress, "Unknown", "Offline");
-                        //}
-                        myPing.Dispose();
-                    });
-                    myThread.Start();
-                    myThread.Join(100);
-                }
+                            //lbStatus.ForeColor = System.Drawing.Color.Green; //Set status label for current IP address
+                            //lbStatus.Text = "Scanning: " + ipAddress;
+                            //Log pinged IP address in listview
+                            //Grabs DNS information to obtain system info
+                            if (reply.Status == IPStatus.Success)
+                            {
+                                try
+                                {
+                                    addr = IPAddress.Parse(ipAddress);
+                                    host = Dns.GetHostEntry(addr);
+                                    data.Rows.Add(addr, host.HostName, "Active");
+                                }
+                                catch
+                                {
 
-                startIP[3] = 1; //If 4th octet reaches 255, reset back to 1
-            }
+                                    data.Rows.Add(IPAddress.Parse(ipAddress), "Unknown", "Active");
+                                }
+                            }
+                            //else
+                            //{
+                            //    data.Rows.Add(ipAddress, "Unknown", "Offline");
+                            //}
+                            myPing.Dispose();
+                            
+                        });
+                        myThread.Start();
+                        myThread.Join(timeout);
+                        progressBar.Value++;
+                    }
+                    startIP[3] = 1; //If 4th octet reaches 255, reset back to 1
+                }
+            });
+            progressBar.Value = 0;
             btnStop.Enabled = false;
             button2.Enabled = true;
             lbStatus.ForeColor = Color.Green;
